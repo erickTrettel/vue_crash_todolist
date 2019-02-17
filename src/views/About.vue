2d81d73734c145ada3ca8fad8c805894d00f93ac
Erick Trettel de Almeida <ericktrettel@gmail.com>
2019-02-17 18:58:53 -0300

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the Todo app v1.0.0. It is part of the Vue crash course on YouTube.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+
+<style scoped>
+  * {
+    padding: 10px;
+  }
+</style>
